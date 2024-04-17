1b6e2083a74a756a1ef3d04ad9107f40e3add3e4
Syed Abdul Moz Shah <83330664+vampire32@users.noreply.github.com>
2024-04-17 06:18:34 -0700

    Initial commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MyBlog</title>
+</head>
+<body>
+    <h1>{{$post->title}}</h1>
+    <div>{{$post->body}}</div>
+
+</body>
+</html>
---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+Route::resource('posts', PostController::class)->only([
+    'destroy','show','store','update'
+]);
+?>
